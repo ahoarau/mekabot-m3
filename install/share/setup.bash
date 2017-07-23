788d5472162e09da33f653bcc0811595bfb33319
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-07-23 18:57:48 +0200

    remove install

--- install/share/setup.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This file configures the environement variables for mekabot
-export M3_CMAKE_MODULES=/home/hoarau/dev/mekabot/meka-cmake-modules/Modules
-export PATH=/home/hoarau/dev/mekabot/install/bin:$PATH
-export LD_LIBRARY_PATH=/home/hoarau/dev/mekabot/install/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/home/hoarau/dev/mekabot/install/lib/python2.7/dist-packages:/home/hoarau/dev/mekabot/install/lib/python2.7/dist-packages/m3:/home/hoarau/dev/mekabot/install/lib/python2.7/dist-packages/m3rt:/home/hoarau/dev/mekabot/install/lib/python2.7/dist-packages/m3qa:$PYTHONPATH
